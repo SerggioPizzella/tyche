67bebbb21b5ea052242cc3aa860653157e3c5cbe
Serggio Pizzella <serggiopizzella@gmail.com>
2023-12-14 13:54:48 +0100

    client can make a character

--- tyche-client/src/main.rs
+++ tyche-client/src/main.rs
@@ -2,12 +2,16 @@
 mod menu;
 
 use bevy::prelude::*;
+use bevy_egui::{egui, EguiContexts, EguiPlugin};
+use serde::{Deserialize, Serialize};
 
 fn main() {
     App::new()
         .add_state::<GameState>()
         .add_plugins((DefaultPlugins, menu::MenuPlugin))
+        .add_plugins(EguiPlugin)
         .add_systems(Startup, setup)
+        .add_systems(Update, ui_example_system)
         .run();
 }
 
@@ -25,4 +29,33 @@ enum GameState {
     #[default]
     Menu,
     Game,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Character {
+    name: String,
+}
+
+impl Character {
+    fn new(name: String) -> Self {
+        Self { name }
+    }
+}
+
+fn ui_example_system(mut contexts: EguiContexts, mut ui_state: Local<String>) {
+    egui::Window::new("Hello").show(contexts.ctx_mut(), |ui| {
+        ui.horizontal(|ui| {
+            ui.label("Write a name: ");
+            ui.text_edit_singleline(&mut *ui_state);
+        });
+
+        if ui.button("Create character").clicked() {
+            let client = reqwest::blocking::Client::new();
+            _ = client
+                .post("localhost:8000/")
+                .header("Content-Type", "application/json")
+                .json(&Character::new((*ui_state).clone()))
+                .send();
+        }
+    });
 }
